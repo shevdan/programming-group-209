b3a66db33f90bb47a6564123f983cd9b9ba29fcb
hlovatskyibohdan <69348335+hlovatskyibohdan@users.noreply.github.com>
2020-11-03 15:25:19 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,27 @@
 import generate_grid
 import number_type
+from random import choice
 
 if __name__ == '__main__':
     print('blabla (Introduction)')
     mode = input('What play mode do you want to try?')
-    #TODO: decide_mode()
-    grid = generate_grid.generate_grid(0, 100)
-    print(grid)
-    correct_nums = []
+    # TODO: asks the user what level of difficilty (range_of_numbers)
+    # TODO: decide_mode()
+    # TODO: game_mode: limited number of errors, infinite mode
     while True:
+        grid = generate_grid.generate_grid(level_of_difficulty, game_mode)
+        number_types = ['ulam', 'prime', 'happy']
+        print(grid)
+        type_of_num = choice(number_types)
+        print(f'Input {type_of_num}')
+        correct_nums = []
         num_of_user = input('Enter your number')
         if num_of_user:
+            if check_type(type_of_num, num_of_user)
             correct_nums.append(num_of_user)
-            #if check_type():
-            #    result += 1
         else:
             break
+            # ? result -= 1
+
     print(correct_nums)
+    results()
