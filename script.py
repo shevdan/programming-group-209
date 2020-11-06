061e5724bc87637b09bd9952edb31846b21aaa83
bbryk <73888650+bbryk@users.noreply.github.com>
2020-11-06 17:56:53 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import generate_grid
 import number_type
+import interaction_with_player
 from random import choice
 from number_type import user_number_type as check_type
 
@@ -9,8 +10,7 @@ def main():
 
 
 if __name__ == '__main__':
-    # gonna be changed for introduction func.
-    print('blabla (Introduction)')
+    interaction_with_player.start_intro()
 
     correct_nums = []
     num_of_lives = 3
@@ -18,26 +18,22 @@ if __name__ == '__main__':
     while True:
         range_of_nums = generate_grid.level_of_dif(num_of_iterations)
         grid = generate_grid.generate_grid(range_of_nums)
-        number_types = ['ulam', 'prime', 'lucky']
-        print(grid)
-        type_of_num = choice(number_types)
-        print(f'Input {type_of_num}')
-        try:
-            num_of_user = int(input('Enter your number'))
-        except ValueError:
-            break
+        
+        type_of_num = interaction_with_player.form_num(grid)
+        num_of_user = interaction_with_player.user_num()
+
+
         user_nums = []
-        num_of_iterations += 1
+        num_of_iterations += 1 # цей рядок мб мав би бути після першого іфа
         if num_of_user:
             if check_type(num_of_user, type_of_num):
-                print('yeah')
+                interaction_with_player.congrats_output()
                 correct_nums.append(num_of_user)
             else:
                 num_of_lives -= 1
                 if num_of_lives <= 0:
-                    print('You lost and gonna die in poverty')
+                    interaction_with_player.mistake_output(num_of_lives)
                     break
-        
         else:
             break
 
