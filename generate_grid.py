f5da54122e2c7fa8431a480084897ef3741ac4d1
hlovatskyibohdan <69348335+hlovatskyibohdan@users.noreply.github.com>
2020-11-06 15:59:11 +0200

    Update generate_grid.py
    Added levels of difficulty

--- generate_grid.py
+++ generate_grid.py
@@ -1,9 +1,28 @@
 from random import sample
 
-range_of_nums = [i for i in range(100)]
+def level_of_dif(num_of_iterations):
+    '''
+    This functions determines the range
+    from which the numbers will be taken.
+
+    0 - 3 iterations : easy level of game: range of numbers [0, 20]
+    4 - 6 iterations : medium level of game: range of numbers [20, 50]
+    7 - 9 iterations : hard level of game: range of numbers [50, 100]
+    '''
+
+    if -1 < num_of_iterations < 4:
+        return [10, 20]
+    if 3 < num_of_iterations < 7:
+        return [20, 50]
+    if 6 < num_of_iterations < 10:
+        return [50, 100]
 
 
-def generate_grid(from_num: int, to_num: int) -> list:
-    grid = sample(range_of_nums, 10)
+def generate_grid(range_of_nums) -> list:
+    '''
+    '''
+
+    grid = sample([i for i in range(range_of_nums[0], range_of_nums[1])], 10)
+
 
     return grid
