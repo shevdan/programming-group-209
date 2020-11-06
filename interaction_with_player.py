dedc102332be3f1d6205d2c00649d01f6fb668b4
bbryk <73888650+bbryk@users.noreply.github.com>
2020-11-06 14:01:20 +0200

    Update interaction_with_player.py

--- interaction_with_player.py
+++ interaction_with_player.py
@@ -5,7 +5,19 @@ module that contains functions to interact with user
 def start_intro():
     print("Today u r having very important test , Let's start.")
 
+def lvl_of_difficuly():
+    lvl = input("Choose the level of difficuly :\nEazy Medium Hard\n")
+    if lvl == 'Eazy':
+        return (0,20)
+    elif lvl == 'Medium':
+        return (21,50)
+    elif lvl == 'Hard':
+        return (51,100)
+    else:
+        print("Please , input correct level of difficulty:")
+        lvl_of_difficuly()
+
 def game_round():
     number_types = ['ulam', 'prime', 'happy']
     num_type = choice(number_types)
-    print(f'Please, choose the {num_type} from the following:')
+    print(f'Please, choose the {num_type} number from the following:')
