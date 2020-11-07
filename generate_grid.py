4f30119597e04ba7f163e200f738b280019d892b
shevdan <70774719+shevdan@users.noreply.github.com>
2020-11-07 16:24:12 +0200

    Update generate_grid.py

--- generate_grid.py
+++ generate_grid.py
@@ -1,10 +1,10 @@
-from random import sample
+from random import sample, randint
+from number_type import user_number_type as check
 
 def level_of_dif(num_of_iterations):
     '''
     This functions determines the range
     from which the numbers will be taken.
-
     0 - 3 iterations : easy level of game: range of numbers [0, 20]
     4 - 6 iterations : medium level of game: range of numbers [20, 50]
     7 - 9 iterations : hard level of game: range of numbers [50, 100]
@@ -18,11 +18,15 @@ def level_of_dif(num_of_iterations):
         return [50, 100]
 
 
-def generate_grid(range_of_nums) -> list:
+def generate_grid(range_of_nums, num_type) -> list:
     '''
     '''
+    right_num = randint(range_of_nums[0], range_of_nums[1])
+    while not check(right_num, num_type):
+        right_num = randint(range_of_nums[0], range_of_nums[1])
 
-    grid = sample([i for i in range(range_of_nums[0], range_of_nums[1])], 10)
+    grid = sample([i for i in range(range_of_nums[0], range_of_nums[1])], 9)
+    grid.append(right_num)
 
 
     return grid
