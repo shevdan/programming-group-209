fa8451037701c3d0823bd0ae1c0d035fbcecc06b
hlovatskyibohdan <69348335+hlovatskyibohdan@users.noreply.github.com>
2020-11-08 18:37:54 +0200

    Update generate_grid.py

--- generate_grid.py
+++ generate_grid.py
@@ -1,4 +1,16 @@
+'''
+This module contains functions that implements generation of the game grid.
+
+Function level_of_dif determines the range (which is the representation
+of the level of difficulty which that will be increased thorough the test)
+from which the numbers will be taken.
+
+Function generate_grid generates grid, with a one specific number of needed
+type and 9 more random numbers.
+'''
+
 from random import sample, randint, shuffle
+from typing import List
 from number_type import user_number_type as check
 
 def level_of_dif(num_of_iterations):
@@ -10,22 +22,39 @@ def level_of_dif(num_of_iterations):
     7 - 9 iterations : hard level of game: range of numbers [50, 100]
     '''
 
+    range_of_nums = []
     if -1 < num_of_iterations < 4:
-        return [10, 20]
+        range_of_nums = [10, 20]
     if 3 < num_of_iterations < 7:
-        return [20, 50]
+        range_of_nums = [20, 50]
     if 6 < num_of_iterations < 10:
-        return [50, 100]
+        range_of_nums = [50, 100]
+
+    return range_of_nums
 
 
-def generate_grid(range_of_nums, num_type) -> list:
+def generate_grid(range_of_nums: List[int], num_type: str) -> List[int]:
     '''
+    This function generates the game grid, which consist of 10 numbers.
+
+    Args : range_of_nums: a list of two ints, which represent the level of difficulty,
+                which increases thorough the test.
+            num_type: a string, which represents what type of num has to be present in
+                the game grid.
+
+    Returns: a list of 10 positive ints, which represents the game grid.
+
+    Args are given by the other functions, therefore no exceptions should be rose.
     '''
+
     right_num = randint(range_of_nums[0], range_of_nums[1])
+
+    # checks whether a num of desired type will be present in the grid.
     while not check(right_num, num_type):
         right_num = randint(range_of_nums[0], range_of_nums[1])
 
-    grid = sample([i for i in range(range_of_nums[0], range_of_nums[1])], 9)
+    range_of_generation = [i for i in range(range_of_nums[0], range_of_nums[1])]
+    grid = sample(range_of_generation, 9)
     grid.append(right_num)
     shuffle(grid)
 
