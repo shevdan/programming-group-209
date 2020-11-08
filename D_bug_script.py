b0b557852587b5c2bd1d938557155469ca3c48da
shevdan <70774719+shevdan@users.noreply.github.com>
2020-11-08 20:53:16 +0200

    Update and rename Big_B_script.py to D_bug_script.py

--- D_bug_script.py
+++ D_bug_script.py
@@ -3,9 +3,9 @@ This module contains main function (one iteration of game)
 and ascript to run the game
 '''
 
-import generate_grid
-import interaction_with_player
-from number_type import user_number_type as check_type
+import D_bug_generate_grid
+import D_bug_interaction_with_player
+from D_bug_number_type import user_number_type as check_type
 
 def one_iteration(num_of_lives: int, num_of_iterations: int) -> bool:
     '''
@@ -16,22 +16,26 @@ def one_iteration(num_of_lives: int, num_of_iterations: int) -> bool:
     '''
 
     if num_of_iterations[0] == 9:
-        interaction_with_player.victory_output()
+        D_bug_interaction_with_player.victory_output()
         return False
 
-    range_of_nums = generate_grid.level_of_dif(num_of_iterations[0])
-    type_of_num = interaction_with_player.form_num()
-    grid = generate_grid.generate_grid(range_of_nums, type_of_num)
+    range_of_nums = D_bug_generate_grid.level_of_dif(num_of_iterations[0])
+    type_of_num = D_bug_interaction_with_player.form_num()
+    grid = D_bug_generate_grid.generate_grid(range_of_nums, type_of_num)
     print(grid)
-    num_of_user = interaction_with_player.user_num()
+    try:
+        num_of_user = D_bug_interaction_with_player.user_num()
+    except EOFError:
+        print('You lost... You were so close...')
+        return None
 
     if num_of_user:
         if check_type(num_of_user, type_of_num) and num_of_user in grid:
             num_of_iterations[0] += 1
-            interaction_with_player.congrats_output()
+            D_bug_interaction_with_player.congrats_output()
         else:
             num_of_lives[0] -= 1
-            interaction_with_player.mistake_output(num_of_lives)
+            D_bug_interaction_with_player.mistake_output(num_of_lives)
             # breaks the execution of the function, if the number of lives == 0
             if num_of_lives[0] <= 0:
                 return False
@@ -42,7 +46,7 @@ def one_iteration(num_of_lives: int, num_of_iterations: int) -> bool:
 
 if __name__ == '__main__':
 
-    interaction_with_player.start_intro()
+    D_bug_interaction_with_player.start_intro()
     num_of_lives_in_game = [3]
     num_of_iterations_of_game = [0]
 
